daff93957367cdd2dbef3580a52584d6b62edf0b
Mohammad Shohan <shohan11d@gmail.com>
2024-03-06 20:35:42 +0600

    watchedmovie function added

--- src/App-v2.js
+++ src/App-v2.js
@@ -475,3 +475,33 @@ function WatchedMovie({ movie, onDeleteWatched }) {
   );
 }
 /* new section here */
+
+function WatchedMovie({ movie, onDeleteWatched }) {
+  return (
+    <li>
+      <img src={movie.poster} alt={`${movie.title} poster`} />
+      <h3>{movie.title}</h3>
+      <div>
+        <p>
+          <span>⭐️</span>
+          <span>{movie.imdbRating}</span>
+        </p>
+        <p>
+          <span>🌟</span>
+          <span>{movie.userRating}</span>
+        </p>
+        <p>
+          <span>⏳</span>
+          <span>{movie.runtime} min</span>
+        </p>
+
+        <button
+          className="btn-del"
+          onClick={() => onDeleteWatched(movie.imdbID)}
+        >
+          X
+        </button>
+      </div>
+    </li>
+  );
+}
