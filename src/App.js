1ff809828035b6712269a310c15b97c3eb6caac5
shohan11d <shohan11d@gmail.com>
2024-08-18 04:42:53 +0600

    navbar done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,61 @@
+import { useRef, useState } from "react";
+import {useKey} from "./useKey"
+import {useMovies} from "./useMovies"
 function App() {
+
+  const [selectedId, setSelectedId] = useState(null);
+  const [query, setQuery] = useState("");
+  const { movies, isLoading, error } = useMovies(query);
+  // const [watched, setWatched] = useLocalStorageState([], "watched");
   return (
-    <div>
-      <p> className="nav-bar">Lorem ipsum dolor sit amet.</p>
-    </div>
+    <NavBar>
+      <Search query={query} setQuery={setQuery} />
+      <NumResults movies={movies} />
+    </NavBar>
+  );
+}
+function Search({ query, setQuery }) {
+  const inputEl = useRef(null);
+
+  useKey("Enter", function () {
+    if (document.activeElement === inputEl.current) return;
+    inputEl.current.focus();
+    setQuery("");
+  });
+
+  return (
+    <input
+      className="search"
+      type="text"
+      placeholder="Search movies..."
+      value={query}
+      onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
+    />
   );
 }
 
+function NumResults({ movies }) {
+  return (
+    <p className="num-results">
+      Found <strong>{movies.length}</strong> results
+    </p>
+  );
+}
+function NavBar({ children }) {
+  return (
+    <nav className="nav-bar">
+      <Logo />
+      {children}
+    </nav>
+  );
+}
+function Logo() {
+  return (
+    <div className="logo">
+      <span role="img">🍿</span>
+      <h1>usePopcorn</h1>
+    </div>
+  );
+}
 export default App;
