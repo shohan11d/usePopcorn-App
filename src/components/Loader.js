3a09569e0bbb3fd4f3b7a0f8ca0b0cb6d188304e
shohan11d <shohan11d@gmail.com>
2024-08-24 17:24:46 +0600

    git reset

--- src/components/Loader.js
+++
@@ -1,6 +0,0 @@
-function Loader() {
-  return <p className="loader">Loading...</p>;
-}
-
-
-export default Loader
--- src/components/Main.js
+++
@@ -1,7 +0,0 @@
-export { Main, Box };
-function Main({ children }) {
-  return <div className="main">{children}</div>;
-}
-function Box({ children }) {
-  return <div className="box">{children}</div>;
-}
